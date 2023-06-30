e3adfadeaf7281960268a27225cebe08237a8246
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-06-30 13:05:39 +0500

    how to type props in TS

---
+++ src/components/Intro.tsx
@@ -0,0 +1,19 @@
+// interface IntroProps {
+//   user_name: string;
+//   age: number;
+// }
+
+type IntroProps = {
+  user_name: string;
+  age: number;
+};
+
+export const Intro = (props: IntroProps) => {
+  return (
+    <div>
+      <h1>
+        Hello, my name is {props.user_name} and i am {props.age} years old
+      </h1>
+    </div>
+  );
+};
